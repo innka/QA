fa5c627c2e8ff62e50875c9969caa0ac2c691858
innka <innka2705@gmail.com>
2024-06-20 16:45:40 +0100

    individual project 5 DataBase

--- python_basic/modules/common/database.py
+++ python_basic/modules/common/database.py
@@ -1,4 +1,5 @@
 import sqlite3
+from datetime import date
 
 class DataBase():#клас для БД
 
@@ -20,6 +21,7 @@ class DataBase():#клас для БД
         self.cursor.execute(query)
         record = self.cursor.fetchall()
         return record
+
     #отримати дані користувача за його іменем
     def get_all_user_by_name(self, name):
         query = f"SELECT address, city, postalCode, country FROM customers WHERE name = '{name}'"
@@ -36,6 +38,12 @@ class DataBase():#клас для БД
     #перед оновлення зробити виборку даних
     def select_product_qnt_by_id(self, product_id):
         query = f"SELECT quantity FROM products WHERE id = {product_id}"
+        self.cursor.execute(query)
+        record = self.cursor.fetchall()
+        return record
+    
+    def get_product_by_name(self, product_name):
+        query = f"SELECT * FROM products WHERE name = {product_name}"
         self.cursor.execute(query)
         record = self.cursor.fetchall()
         return record
@@ -61,3 +69,130 @@ class DataBase():#клас для БД
         self.cursor.execute(query)
         record = self.cursor.fetchall()
         return record
+    
+    """
+    individual project party
+    """
+    #отримати усі продукти
+    def get_all_products(self):
+        query = "SELECT * FROM products"
+        self.cursor.execute(query)
+        record = self.cursor.fetchall()
+        return record
+
+       #отримати усі замовлення
+    def get_all_orders(self):
+        query = "SELECT * FROM orders"
+        self.cursor.execute(query)
+        record = self.cursor.fetchall()
+        return record
+    
+    def get_custumer_by_id(self, custumer_id):
+        query = f"SELECT * FROM customers WHERE id = {custumer_id}"
+        self.cursor.execute(query)
+        record = self.cursor.fetchall()
+        return record
+        
+    
+    def insert_custumers(self,custumer_id, name, address, city, postal_code, country):
+        if not isinstance(name, str) or not isinstance(address, str) or not isinstance(city, str) or not isinstance(postal_code, str) or not isinstance(country, str):
+             raise ValueError("Invalid data type provided")
+        else:
+            query = f"INSERT OR REPLACE INTO customers (id, name, address, city, postalCode, country) \
+                VALUES ({custumer_id}, '{name}', '{address}', '{city}', '{postal_code}', '{country}')"
+            self.cursor.execute(query)
+            self.connection.commit()#підтвердження змін в БД
+    
+    def update_custumers_name(self, custumer_id,name):
+        if not isinstance(name, str) or name == "":
+             raise ValueError("Invalid data type provided")
+        else:
+            query = f"UPDATE customers SET name = '{name}' WHERE id = {custumer_id}"
+            self.cursor.execute(query)
+            self.connection.commit()#підтвердження змін в БД, щоб перевірити коректність роботи 
+    
+    def update_custumers_address(self, custumer_id,address):
+        if not isinstance(address, str) or address == "":
+             raise ValueError("Invalid data type provided")
+        else:
+            query = f"UPDATE customers SET address = '{address}' WHERE id = {custumer_id}"
+            self.cursor.execute(query)
+            self.connection.commit()#підтвердження змін в БД, щоб перевірити коректність роботи 
+
+    def update_custumers_city(self, custumer_id,city):
+        if not isinstance(city, str) or city == "":
+             raise ValueError("Invalid data type provided")
+        else:
+            query = f"UPDATE customers SET city = '{city}' WHERE id = {custumer_id}"
+            self.cursor.execute(query)
+            self.connection.commit()#підтвердження змін в БД, щоб перевірити коректність роботи 
+
+    def update_custumers_postalCode(self, custumer_id,postalCode):
+        if not isinstance(postalCode, str) or postalCode == "":
+             raise ValueError("Invalid data type provided")
+        else:
+            query = f"UPDATE customers SET postalCode = '{postalCode}' WHERE id = {custumer_id}"
+            self.cursor.execute(query)
+            self.connection.commit()#підтвердження змін в БД, щоб перевірити коректність роботи 
+
+    def update_custumers_country(self, custumer_id,country):
+        if not isinstance(country, str) or country == "":
+             raise ValueError("Invalid data type provided")
+        else:
+            query = f"UPDATE customers SET country = '{country}' WHERE id = {custumer_id}"
+            self.cursor.execute(query)
+            self.connection.commit()#підтвердження змін в БД, щоб перевірити коректність роботи 
+
+
+    def delete_custumers_by_id(self, custumer_id):
+        query = f"DELETE FROM customers WHERE id = {custumer_id}"
+        self.cursor.execute(query)
+        self.connection.commit()
+
+    def get_detailed_order_by_id(self, order_id):
+        query = f"SELECT orders.id, customers.name, products.name, \
+            products.description, orders.order_date \
+                FROM orders \
+                    JOIN customers ON orders.customer_id = customers.id\
+                        JOIN products ON orders.product_id = products.id\
+                            WHERE orders.id = {order_id}"
+        self.cursor.execute(query)
+        record = self.cursor.fetchall()
+        return record
+    
+    def insert_new_order_2(self,order_id,customer_name, product_name, order_date):
+        if  not isinstance(order_id, int) or not isinstance(customer_name, str) or not isinstance(product_name, str) or not isinstance(order_date, date):
+            raise ValueError("Invalid data type provided")
+        else:
+            query = f"INSERT OR REPLACE INTO orders (id, customer_id, product_id, order_date)\
+                VALUES ({order_id}, \
+                (SELECT id FROM customers WHERE name = '{customer_name}'), \
+                (SELECT id FROM products WHERE name = '{product_name}'), \
+                {order_date})" 
+            self.cursor.execute(query)
+            self.connection.commit()#підтвердження змін в БД
+
+    def insert_new_order(self,order_id,customer_id, product_id, order_date):
+        if not isinstance(order_id, int) or not isinstance(customer_id, int) or not isinstance(product_id, int) or not isinstance(order_date, date):
+            raise ValueError("Invalid data type provided")
+        else:
+            query = f"INSERT OR REPLACE INTO orders (id, customer_id, product_id, order_date) \
+            VALUES ({order_id}, {customer_id}, {product_id}, {order_date})" 
+            self.cursor.execute(query)
+            self.connection.commit()#підтвердження змін в БД"""
+
+    def update_orders_by_id_customer_name(self, order_id, customer_name):
+        if not isinstance(order_id, int) or not isinstance(customer_name, str):
+            raise ValueError("Invalid data type provided")
+        else:
+            query = f"UPDATE orders SET customer_id = (SELECT id FROM customers WHERE name = '{customer_name}') WHERE id = {order_id}"
+            self.cursor.execute(query)
+            self.connection.commit()#підтвердження змін в БД, щоб перевірити коректність роботи 
+    
+    def update_orders_by_id_product_name(self, order_id, product_name):
+        if not isinstance(order_id, int) or not isinstance(product_name, str):
+            raise ValueError("Invalid data type provided")
+        else:
+            query = f"UPDATE orders SET product_id = (SELECT id FROM products WHERE name = '{product_name}') WHERE id = {order_id}"
+            self.cursor.execute(query)
+            self.connection.commit()#підтвердження змін в БД, щоб перевірити коректність роботи 
--- python_basic/tests/database/test_database.py
+++ python_basic/tests/database/test_database.py
@@ -1,6 +1,6 @@
 import pytest
 from modules.common.database import DataBase
-
+from datetime import date
 @pytest.mark.database
 def test_database_connection():
     db = DataBase()#екземпляр класу ДБ
@@ -10,9 +10,8 @@ def test_database_connection():
 def test_check_all_users():
     db = DataBase()
     users = db.get_all_users()
-
     print(users)
-
+    
 @pytest.mark.database
 def test_check_user_name():
     db = DataBase()
@@ -44,6 +43,7 @@ def test_product_delete():
     db.delete_product_by_id(99)
     qnt = db.select_product_qnt_by_id(99)
     assert len(qnt) == 0# довжина отриманого масиву = 0, тобто пустота
+
 @pytest.mark.database
 def test_detqiled_orders():
     db = DataBase()
@@ -57,3 +57,135 @@ def test_detqiled_orders():
     assert orders[0][1] == 'Sergii'
     assert orders[0][2] == 'солодка вода'
     assert orders[0][3] =='з цукром'
+
+    """
+    individual project party
+    """
+    """
+    тести що вже є: оновити к-сть товару / додати новий товар / видалити продукт /
+    """
+@pytest.mark.idv_project5
+def test_check_all_products():
+    db = DataBase()
+    products = db.get_all_products()
+    #print("Продукти")
+    #print(products)
+    #print("\n")
+
+@pytest.mark.idv_project5
+def test_check_all_orders():
+    db = DataBase()
+    orders = db.get_all_orders()
+    print("Замовленя")
+    print(orders)
+    print("\n")
+
+@pytest.mark.idv_project5
+def test_insert_costumers():
+    db = DataBase()
+    db.insert_custumers(99,"Andrii","Sobornosti 5", "Kyiv", "69007", "Ukraine")
+    new_custumer = db.get_custumer_by_id(99)
+    #print(new_custumer)
+    assert new_custumer[0][0] == 99
+    assert new_custumer[0][1] == "Andrii"
+    assert new_custumer[0][2] == "Sobornosti 5"
+    assert new_custumer[0][3] == "Kyiv"
+    assert new_custumer[0][4] == "69007"
+    assert new_custumer[0][5] == "Ukraine"
+
+@pytest.mark.idv_project5
+def test_update_name_custumer():
+    db = DataBase()
+    n_name = "Irina"#failed with n_name=123 and n_name=""
+    db.update_custumers_name(99, n_name)
+    new_name = db.get_custumer_by_id(99)# перевірка
+    #print(new_name)
+    assert new_name[0][0] == 99
+    assert new_name[0][1] == n_name
+
+@pytest.mark.idv_project5
+def test_update_custumers_address():
+    db = DataBase()
+    n_address = "Mazepi 8" #failed with n_adress=123 and n_adress=""
+    db.update_custumers_address(99,n_address)
+    new_address = db.get_custumer_by_id(99)
+    #print(new_address)
+    assert new_address[0][0] == 99
+    assert new_address[0][2] == n_address
+
+@pytest.mark.idv_project5
+def test_update_custumers_city():
+    db = DataBase()
+    n_city = "Yaremche" #failed with n_city=123 and n_city=""
+    db.update_custumers_city(99,n_city)
+    new_city = db.get_custumer_by_id(99)
+    #print(new_city)
+    assert new_city[0][0] == 99
+    assert new_city[0][3] == n_city
+
+@pytest.mark.idv_project5
+def test_update_custumers_postalCode():
+    db = DataBase()
+    n_postalCode = "69001" #failed with n_postalCode=123 and n_postalCode=""
+    db.update_custumers_postalCode(99,n_postalCode)
+    new_postalCode = db.get_custumer_by_id(99)
+    #print(new_postalCode)
+    assert new_postalCode[0][0] == 99
+    assert new_postalCode[0][4] == n_postalCode
+
+@pytest.mark.idv_project5
+def test_update_custumers_country():
+    db = DataBase()
+    n_country = "France" #failed with n_country=123 and n_country=""
+    db.update_custumers_country(99,n_country)
+    new_country = db.get_custumer_by_id(99)
+    #print(new_country)
+    assert new_country[0][0] == 99
+    assert new_country[0][5] == n_country
+
+@pytest.mark.idv_project5
+def test_delete_custumers_by_id():
+    db = DataBase()
+    db.insert_custumers(999,"Andrii","Sobornosti 5", "Kyiv", "69007", "Ukraine")
+    db.delete_custumers_by_id(999)
+    del_custumer = db.get_custumer_by_id(999)
+    assert len(del_custumer) == 0
+
+@pytest.mark.idv_project5
+def test_insert_new_order():
+    db = DataBase()
+    db.insert_new_order(99,2,3,date.today())
+    new_order= db.get_detailed_order_by_id(99)
+    #print(new_order)
+    assert new_order[0][0] == 99
+    
+
+@pytest.mark.idv_project5
+def test_insert_new_order_2():
+    db = DataBase()
+    db.insert_new_order_2(98,"Sergii","молоко",date.today())
+    new_order= db.get_detailed_order_by_id(98)
+    assert new_order[0][0] == 98
+    assert new_order[0][1] == "Sergii"
+    assert new_order[0][2] == "молоко"
+
+@pytest.mark.idv_project5
+def test_update_orders_by_id_customer_name():
+    db = DataBase()
+    n_name = "Stepan"
+    db.update_orders_by_id_customer_name(98, n_name)
+    new_order_customer = db.get_detailed_order_by_id(98)
+    print(new_order_customer)
+    assert new_order_customer[0][0] == 98
+    assert new_order_customer[0][1] == n_name
+
+@pytest.mark.idv_project5
+def test_update_orders_by_id_product_name():
+    db = DataBase()
+    n_product = "круасан"
+    db.update_orders_by_id_product_name(98, n_product)
+    new_order_product = db.get_detailed_order_by_id(98)
+    print(new_order_product)
+    assert new_order_product[0][0] == 98
+    assert new_order_product[0][2] == n_product 
+    
